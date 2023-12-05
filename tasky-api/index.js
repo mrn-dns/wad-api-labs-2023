b2c9ce9565e7e16416f7a55cbfeb2204bf718389
Denis <marincasdenis@yahoo.com>
2023-12-05 00:53:13 +0000

    Mongo lab Users Router

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -2,6 +2,7 @@ import './db';
 import dotenv from 'dotenv';
 import express from 'express';
 import tasksRouter from './api/tasks';
+import usersRouter from './api/users'
 
 dotenv.config();
 
@@ -21,6 +22,8 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use('/api/tasks', tasksRouter);
 app.use(errHandler);
+//Users router
+app.use('/api/users', usersRouter);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
