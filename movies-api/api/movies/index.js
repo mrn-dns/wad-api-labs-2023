af857248e0a0782fa9c5a5d514033c3f89eda748
Denis <marincasdenis@yahoo.com>
2023-12-07 23:48:51 +0000

    Added week 11 exercise (genres)

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -1,4 +1,4 @@
-import { getUpcomingMovies } from '../tmdb-api';
+import { getUpcomingMovies, getGenres } from '../tmdb-api';
 import movieModel from './movieModel';
 import asyncHandler from 'express-async-handler';
 import express from 'express';
@@ -40,6 +40,11 @@ router.get('/:id', asyncHandler(async (req, res) => {
 router.get('/tmdb/upcoming', asyncHandler(async (req,res) => {
     const upcomingMovies = await getUpcomingMovies();
     res.status(200).json(upcomingMovies);
+}));
+
+router.get('/tmdb/genres', asyncHandler(async (req,res) => {
+    const genres = await getGenres();
+    res.status(200).json(genres);
 }));
 
 export default router;
